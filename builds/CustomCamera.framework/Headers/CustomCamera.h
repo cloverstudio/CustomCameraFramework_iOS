7adf32e048e567c1fafdb3fe13451cc299c6c0a6
Ivo Peric <peric@clover.studio>
2019-08-21 13:54:28 +0200

    iniatial 2

---
+++ builds/CustomCamera.framework/Headers/CustomCamera.h
@@ -0,0 +1,18 @@
+//
+//  CustomCamera.h
+//  CustomCamera
+//
+//  Created by Ivo Peric on 12/03/2019.
+//  Copyright © 2019 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomCamera.
+FOUNDATION_EXPORT double CustomCameraVersionNumber;
+
+//! Project version string for CustomCamera.
+FOUNDATION_EXPORT const unsigned char CustomCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomCamera/PublicHeader.h>
+
